121e94dd31484b238e41e6cfbb852f4001e95461
Emin Polat <eminpolat749@gmail.com>
2023-07-18 17:32:33 +0300

    second commit

--- src/org/csystem/util/math/geometry/Circle.java
+++ src/org/csystem/util/math/geometry/Circle.java
@@ -2,17 +2,6 @@ package org.csystem.util.math.geometry;
 
 public class Circle {
     private double m_radius;
-    private double m_area, m_perimeter;
-
-    private void calculateArea()
-    {
-        m_area = Math.PI * m_radius * m_radius;
-    }
-
-    private void calculatePerimeter()
-    {
-        m_perimeter = Math.PI * m_radius * 2;
-    }
 
     public Circle()
     {
@@ -28,23 +17,21 @@ public class Circle {
     public void setRadius(double radius)
     {
         m_radius = Math.abs(radius);
-        calculateArea();
-        calculatePerimeter();
     }
 
     public double getArea()
     {
-        return m_area;
+        return Math.PI * m_radius * m_radius;
     }
 
     public double getPerimeter()
     {
-        return m_perimeter;
+        return Math.PI * m_radius * 2;
     }
 
     public String toString()
     {
-        return String.format("Radius:%f, Area:%f, Perimeter:%f%n", m_radius, m_area, m_perimeter);
+        return String.format("Radius:%f, Area:%f, Perimeter:%f%n", m_radius, getArea(), getPerimeter());
     }
 
 
